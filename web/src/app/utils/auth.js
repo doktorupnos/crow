2edba5941d57ea18ae3eb664c1a9a2134c109624
doktorupnos <stefanosdoukas1@yahoo.com>
2023-12-03 15:50:49 +0200

    Endpoint typo

--- web/src/app/utils/auth.js
+++ web/src/app/utils/auth.js
@@ -38,7 +38,7 @@ export const userRegister = async (post) => {
 export const userValid = async () => {
 	var userValid = false;
 	await axios
-		.post(process.env.authValidEndpoint, {}, { withCredentials: true })
+		.post(process.env.authValidEndPoint, {}, { withCredentials: true })
 		.then((response) => {
 			if (response.status == 200) {
 				userValid = true;
