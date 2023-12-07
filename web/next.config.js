451826e7f17ba89ecadb826df2fad99755bbf3b7
Stefanos Doukas <138266673+doktorupnos@users.noreply.github.com>
2023-12-07 21:31:29 +0200

    Secure endpoints
    Bug fix - Browser declines cookies

--- web/next.config.js
+++ web/next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
 	env: {
-		authRegEndPoint: "//0.0.0.0:8000/users",
-		authLoginEndPoint: "//0.0.0.0:8000/login",
-		postGetEndPoint: "//0.0.0.0:8000/posts",
-		authValidEndPoint: "//0.0.0.0:8000/admin/jwt",
+		authRegEndPoint: "//localhost:8000/users",
+		authLoginEndPoint: "//localhost:8000/login",
+		postGetEndPoint: "//localhost:8000/posts",
+		authValidEndPoint: "//localhost:8000/admin/jwt",
 	},
 	reactStrictMode: false,
 };
