245ddfa1391fe8a3cc48af552bb1b797b77cdacd
marios-pz <marios-pz@proton.me>
2023-12-08 16:31:39 +0200

    Gather basic components to get the functionality working on profile page.

--- web/next.config.js
+++ web/next.config.js
@@ -5,6 +5,12 @@ module.exports = {
 		authLoginEndPoint: "//localhost:8000/login",
 		postGetEndPoint: "//localhost:8000/posts",
 		authValidEndPoint: "//localhost:8000/admin/jwt",
+        followEndPoint: "//localhost:8000/follow",
+        unfollowEndPoint: "//localhost:8000/unfollow",
+        followingEndPoint: "//localhost:8000/following",
+        followersEndPoint: "//localhost:8000/followers",
+        followingCountEndpoint: "//localhost:8000/following_count",
+        followersCountEndpoint: "//localhost:8000/followers_count",
 	},
 	reactStrictMode: false,
 };
