8c46fa4c9fe46ec8af07a271212098dfd9489731
doktorupnos <stefanosdoukas1@yahoo.com>
2023-12-12 18:38:28 +0200

    Base structure for post creation

--- web/src/app/home/page.js
+++ web/src/app/home/page.js
@@ -1,14 +1,39 @@
 "use client";
 
 import { redirect } from "next/navigation";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { userValid } from "@/app/utils/auth";
 
 import NavBar from "@/components/nav/NavBar/NavBar";
 import PostBlock from "../_components/PostBlock/PostBlock";
 
+import styles from "./page.module.css";
+
 export default function HomePage() {
 	const [session, setSession] = useState(null);
+	const [textValue, setTextValue] = useState("");
+
+	const textareaRef = useRef(null);
+
+	const handleChange = (event) => {
+		const postText = event.target.value;
+		if (postText.trim() !== "") {
+			setTextValue(event.target.value);
+		} else {
+			setTextValue("");
+		}
+	};
+
+	useEffect(() => {
+		const autoResize = () => {
+			if (textareaRef.current) {
+				textareaRef.current.style.height = "auto";
+				textareaRef.current.style.height =
+					textareaRef.current.scrollHeight + "px";
+			}
+		};
+		autoResize();
+	}, [textValue]);
 
 	useEffect(() => {
 		const sessionValid = async () => {
@@ -29,6 +54,20 @@ export default function HomePage() {
 		return (
 			<>
 				<NavBar />
+				<form className={styles.post_block}>
+					<textarea
+						ref={textareaRef}
+						className={styles.post_text}
+						type="text"
+						value={textValue}
+						onChange={handleChange}
+						placeholder="Quoth the raven.."
+						required
+					/>
+					<div>
+						<button className={styles.post_button}>Post</button>
+					</div>
+				</form>
 				<PostBlock />
 			</>
 		);
