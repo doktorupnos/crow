11677a803a0db512e55136453dca74f3d3ae5013
doktorupnos <stefanosdoukas1@yahoo.com>
2024-02-17 00:42:45 +0200

    Window undefined error fix

--- web/src/app/profile/page.js
+++ web/src/app/profile/page.js
@@ -11,62 +11,65 @@ import { validSession } from "@/utils/auth";
 import { fetchProfile } from "@/utils/profile";
 
 const Profile = () => {
-	const query = new URLSearchParams(window.location.search);
-	const user = query.get("u");
+  const [userData, setUserData] = useState({});
+  const [userDataLoad, setUserDataLoad] = useState(null);
 
-	const [userData, setUserData] = useState({});
-	const [userDataLoad, setUserDataLoad] = useState(null);
+  const [session, setSession] = useState(null);
 
-	const [session, setSession] = useState(null);
+  useEffect(() => {
+    const checkSession = async () => {
+      try {
+        let response = await validSession();
+        if (!response) {
+          console.error(`Invalid session!`);
+          return (window.location.href = "/auth");
+        }
+        setSession(true);
+      } catch (error) {
+        console.error(`Invalid session! [${error.message}]`);
+        return (window.location.href = "/auth");
+      }
+    };
+    if (typeof window !== "undefined") {
+      checkSession();
+    }
+  }, []);
 
-	useEffect(() => {
-		const checkSession = async () => {
-			try {
-				let response = await validSession();
-				if (!response) {
-					console.error(`Invalid session!`);
-					return (window.location.href = "/auth");
-				}
-				setSession(true);
-			} catch (error) {
-				console.error(`Invalid session! [${error.message}]`);
-				return (window.location.href = "/auth");
-			}
-		};
-		checkSession();
-	}, []);
+  useEffect(() => {
+    const query = new URLSearchParams(window.location.search);
+    const user = query.get("u");
+    const getUserData = async (user) => {
+      try {
+        let response = await fetchProfile(user);
+        if (Object.keys(response).length > 0) {
+          setUserData(response);
+          setUserDataLoad(true);
+        } else {
+          setUserDataLoad(false);
+        }
+      } catch (error) {
+        console.error(`Failed to fetch user data! [${error.message}]`);
+      }
+    };
+    if (typeof window !== "undefined") {
+      getUserData(user);
+    }
+  }, []);
 
-	useEffect(() => {
-		const getUserData = async (user) => {
-			try {
-				let response = await fetchProfile(user);
-				if (Object.keys(response).length > 0) {
-					setUserData(response);
-					setUserDataLoad(true);
-				} else {
-					setUserDataLoad(false);
-				}
-			} catch (error) {
-				console.error(`Failed to fetch user data! [${error.message}]`);
-			}
-		};
-		getUserData(user);
-	}, [user]);
-
-	return (
-		session && (
-			<>
-				<NavBar />
-				{userDataLoad && (
-					<>
-						<ProfileGrid userData={userData} />
-						<PostGrid user={userData.name} />
-					</>
-				)}
-				{userDataLoad === false && <ErrorUser />}
-			</>
-		)
-	);
+  return (
+    session && (
+      <>
+        <NavBar />
+        {userDataLoad && (
+          <>
+            <ProfileGrid userData={userData} />
+            <PostGrid user={userData.name} />
+          </>
+        )}
+        {userDataLoad === false && <ErrorUser />}
+      </>
+    )
+  );
 };
 
 export default Profile;
