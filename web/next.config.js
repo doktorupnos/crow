eac125ee617ac69de0872fba219712a90fe77e84
marios-pz <marios-pz@proton.me>
2024-04-16 18:53:38 +0300

    Format kubernetes manifests and add production url. Webapp production build is not running, I will fix it on the next commit

--- web/next.config.js
+++ web/next.config.js
@@ -1,17 +1,18 @@
 /** @type {import('next').NextConfig} */
 
 module.exports = {
-	env: {
-		authValidEndPoint: "//localhost:8000/admin/jwt",
-		authRegEndPoint: "//localhost:8000/users",
-		authLoginEndPoint: "//localhost:8000/login",
-		postGetEndPoint: "//localhost:8000/posts",
-		postLikeEndPoint: "//localhost:8000/post_likes",
-		profileEndPoint: "//localhost:8000/profile",
-		followEndPoint: "//localhost:8000/follow",
-		unfollowEndPoint: "//localhost:8000/unfollow",
-		followersEndPoint: "//localhost:8000/followers",
-		followingEndPoint: "//localhost:8000/following",
-	},
-	reactStrictMode: false,
+  output: "standalone",
+  env: {
+    authValidEndPoint: "//localhost:8000/admin/jwt",
+    authRegEndPoint: "//localhost:8000/users",
+    authLoginEndPoint: "//localhost:8000/login",
+    postGetEndPoint: "//localhost:8000/posts",
+    postLikeEndPoint: "//localhost:8000/post_likes",
+    profileEndPoint: "//localhost:8000/profile",
+    followEndPoint: "//localhost:8000/follow",
+    unfollowEndPoint: "//localhost:8000/unfollow",
+    followersEndPoint: "//localhost:8000/followers",
+    followingEndPoint: "//localhost:8000/following",
+  },
+  reactStrictMode: false,
 };
